9049fa9a9365d62354fe46295119485a268ed039
Tiffany Hill <hill.tiffany365@gmail.com>
2023-09-04 15:48:37 -0400

    Created an array and modified the loop to only print the odd numbers in array.

---
+++ control-flow-and-collections/exercises/collections-exercises/src/main/java/org/launchcode/ArrayPractice.java
@@ -0,0 +1,13 @@
+package org.launchcode;
+
+public class ArrayPractice {
+    public static void main(String[] args) {
+        int[] numbers = {1,1,2,3,5,8};
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (i % 2 != 0) {
+                System.out.println(numbers[i]);
+            }
+        }
+    }
+}
