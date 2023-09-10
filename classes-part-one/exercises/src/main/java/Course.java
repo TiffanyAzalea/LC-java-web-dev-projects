9823bd7765d27279b16a9d2e13ef46ade621976d
Tiffany Hill <hill.tiffany365@gmail.com>
2023-09-10 14:32:55 -0400

    Completed 1-4(1/2). Still need to complete 5-7.

---
+++ classes-part-one/exercises/src/main/java/Course.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Course {
+    public static void main(String[] args) {
+
+    }
+    private String topic;
+    private String teacher;
+    private ArrayList<Student> enrolledStudents;
+}
---
+++ classes-part-one/exercises/src/main/java/Teacher.java
@@ -0,0 +1,15 @@
+public class Teacher {
+    private String firstName;
+    private String lastName;
+    private String subject;
+    private String yearsTeaching;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName;
+    }
+
+}
