ccda7df8841475f12d8f3e6e15f795f9b7a134c5
Tiffany Hill <hill.tiffany365@gmail.com>
2023-10-08 21:57:01 -0400

    Bonus Mission 1 syntax added for if user enters a negative number or a non-numeric character

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -5,12 +5,30 @@ public class Area {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+//Basic solution
+//        System.out.println("Enter a radius: ");
+//        double radius = input.nextDouble();
+//        double area = Circle.getArea(radius);
+//        System.out.println("The area of a circle with a radius of " + radius + " is: " + area);
 
+
+      //Bonus Mission 1 syntax added for if user enters a negative number or a non-numeric character
         System.out.println("Enter a radius: ");
-        double radius = input.nextDouble();
-        double area = Circle.getArea(radius);
-        System.out.println("The area of a circle with a radius of " + radius + " is: " + area);
 
+        if (input.hasNextDouble()) {
+            Double radius = input.nextDouble();
+
+            if (radius > 0) {
+                Double area = Circle.getArea(radius);
+                System.out.println("The area of a circle with a radius of " + radius + " is: " + area);
+            } else {
+                System.out.println("Sorry, the radius must be a positive number");
+            }
+        } else {
+            System.out.println("Sorry, that was not a valid number");
+        }
+
+        // while or do-while loop to re-prompt user
 
 
         input.close();
