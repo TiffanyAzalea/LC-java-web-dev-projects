144c5fd0abc6dc6a266a97deaeb737474774e4bc
Tiffany Hill <hill.tiffany365@gmail.com>
2023-10-09 20:15:15 -0400

    Bonus Mission 2 while or do-while loop to re-prompt user

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -15,21 +15,36 @@ public class Area {
       //Bonus Mission 1 syntax added for if user enters a negative number or a non-numeric character
         System.out.println("Enter a radius: ");
 
-        if (input.hasNextDouble()) {
-            Double radius = input.nextDouble();
+//        if (input.hasNextDouble()) {
+//            Double radius = input.nextDouble();
+//
+//            if (radius > 0) {
+//                Double area = Circle.getArea(radius);
+//                System.out.println("The area of a circle with a radius of " + radius + " is: " + area);
+//            } else {
+//                System.out.println("Sorry, the radius must be a positive number");
+//            }
+//        } else {
+//            System.out.println("Sorry, that was not a valid number");
+//        }
 
-            if (radius > 0) {
-                Double area = Circle.getArea(radius);
-                System.out.println("The area of a circle with a radius of " + radius + " is: " + area);
-            } else {
-                System.out.println("Sorry, the radius must be a positive number");
+        //Bonus 2 while or do-while loop to re-prompt user
+
+        Double radius;
+
+        do {
+            System.out.println("Please enter a positive number for the radius: ");
+
+            while (!input.hasNextDouble()){
+                System.out.println("The radius must be a number! Try again!");
+                input.next();
             }
-        } else {
-            System.out.println("Sorry, that was not a valid number");
-        }
+            radius = input.nextDouble();
 
-        // while or do-while loop to re-prompt user
+        } while (radius <= 0);
 
+        double area = Circle.getArea(radius);
+//       System.out.println("The area of a circle with a radius of " + radius + " is: " + area);
 
         input.close();
     }
