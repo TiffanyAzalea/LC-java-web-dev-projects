31074f4be7f1b9aa35c320fb478265ce9a8d3089
Tiffany Hill <hill.tiffany365@gmail.com>
2023-10-09 21:09:26 -0400

    Finished part of the Array section in the Chapter 3 exercise

--- control-flow-and-collections/exercises/collections-exercises/src/main/java/org/launchcode/ArrayPractice.java
+++ control-flow-and-collections/exercises/collections-exercises/src/main/java/org/launchcode/ArrayPractice.java
@@ -5,9 +5,16 @@ public class ArrayPractice {
         int[] numbers = {1,1,2,3,5,8};
 
         for (int i = 0; i < numbers.length; i++) {
-            if (i % 2 != 0) {
+            if (numbers[i] % 2 != 0) {
                 System.out.println(numbers[i]);
             }
         }
+
+        String suess = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
+
+        String[] splitLines = suess.split(" ");
+
+        System.out.println(Arrays.toString(splitLines));
+
     }
 }
